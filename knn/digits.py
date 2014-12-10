2f82aa0b2407ed528f6eff0d24db94d0a8206bcb
enginebai <ericwei.c63amg@gmail.com>
2014-12-10 20:09:48 +0800

    Add digit knn classifier

--- knn/digits.py
+++ knn/digits.py
@@ -3,6 +3,8 @@
 __author__ = 'enginebai'
 
 from numpy import *
+import os
+import kNN
 
 def img2vector(file_name):
     """
@@ -21,3 +23,35 @@ def img2vector(file_name):
                 vector[0, 32 * i + j] = int(line[j])
     return vector
 
+def digit_classifier():
+
+    # training phase
+    digit_labels = []
+    train_file_list = os.listdir('trainingDigits')
+    train_file_num = len(train_file_list)
+    train_vector = zeros((train_file_num, 1024))
+    for i in range(train_file_num):
+        # 檔案全名，包含副檔名
+        file_full_name = train_file_list[i]
+        # 純檔案名稱，不含副檔名
+        file_name = file_full_name.split('.')[0]
+        # 檔案名稱格式是"數字_計數.txt"，這行是要取得數字部分
+        digit_name = int(file_name.split('_')[0])
+        digit_labels.append(digit_name)
+        train_vector[i, :] = img2vector('trainingDigits/%s' % file_full_name)
+
+    # test phase
+    test_file_list = os.listdir('testDigits')
+    error_count = 0.0
+    test_file_num = len(test_file_list)
+    for i in range(test_file_num):
+        file_full_name = test_file_list[i]
+        file_name = file_full_name.split('.')[0]
+        digit_name = int(file_name.split('_')[0])
+        test_vector = img2vector('testDigits/%s' % file_full_name)
+        classify_result = kNN.knn_classify(test_vector, train_vector, digit_labels, 3)
+        print 'Classify result = %s, real answer = %s' % (classify_result, digit_name)
+        if classify_result != digit_name:
+            error_count += 1
+
+    print 'Total error = %d, error rate = %f' % (error_count, (error_count / float(test_file_num)))
