43d022a55dcb1c1d43c8525f0ec9a9344dbff6e6
enginebai <ericwei.c63amg@gmail.com>
2014-12-17 12:35:08 +0800

    Add best feature selection API to decision tree module

--- dtree/tree.py
+++ dtree/tree.py
@@ -26,11 +26,18 @@ def calculate_entropy(dataset):
     return entropy
 
 def create_data_set():
-    data_set = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'yes'], [0, 1, 'yes'], [0, 1, 'y']]
+    data_set = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
     labels = ['no', 'flippers']
     return data_set, labels
 
 def split_data_set(data_set, axis, value):
+    """
+    依照給定的特徵劃分資料集合。
+    :param data_set: 待劃分的資料集合
+    :param axis: 劃分資料集合的特徵值
+    :param value: 需要回傳的特徵值
+    :return:
+    """
     new_data_set = []
     for feature_vector in data_set:
         if feature_vector[axis] == value:
@@ -38,3 +45,23 @@ def split_data_set(data_set, axis, value):
             reduced_feature_vector.extend(feature_vector[axis + 1:])
             new_data_set.append(reduced_feature_vector)
     return new_data_set
+
+def choose_best_feature_to_split(data_set):
+    num_feature = len(data_set[0]) - 1
+    base_entropy = calculate_entropy(data_set)
+    best_info_gain = 0.0
+    best_feature = -1
+    for i in range(num_feature):
+        feature_list = [instance[i] for instance in data_set]
+        unique_value = set(feature_list)
+        new_entropy = 0.0
+        for value in unique_value:
+            sub_data_set = split_data_set(data_set, i, value)
+            prob = len(sub_data_set) / float(len(data_set))
+            new_entropy += prob * calculate_entropy(sub_data_set)
+        info_gain = base_entropy - new_entropy
+        if info_gain > best_info_gain:
+            best_info_gain = info_gain
+            best_feature = i
+
+    return best_feature
